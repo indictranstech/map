a95f8da12afa085f5f11919c23679881f3bce585
Gangadhar Kadam <gangadhar.k@indictanstech.com>
2015-09-14 18:48:26 +0530

    updated js as plugin updated

--- map/locate_your_address_on_map/doctype/locate_address_on_map/locate_address_on_map.js
+++ map/locate_your_address_on_map/doctype/locate_address_on_map/locate_address_on_map.js
@@ -1,4 +1,4 @@
-// frappe.provide("locate_address_on_map")
+
 {% include "locate_your_address_on_map/map_plugin.js" %}
 
 
@@ -10,6 +10,12 @@ frappe.ui.form.on("Locate Address On Map", "refresh", function(frm, dt, dn) {
 
 frappe.ui.form.on("Locate Address On Map", "address_form", function(frm, dt, dn) {
   // trigger on address form field for settings lat lon from postal address
+  if(cur_frm.doc.address_form){
+      get_latlon(cur_frm)  
+  }
+});
+
+function get_latlon(){
     frappe.call({
         method:"map.locate_your_address_on_map.doctype.locate_address_on_map.locate_address_on_map.get_latlon",
             args:{
@@ -22,10 +28,10 @@ frappe.ui.form.on("Locate Address On Map", "address_form", function(frm, dt, dn)
                     cur_frm.doc.address= r.message['address']; 
                     refresh_field(['address', 'lat', 'lon']);
                     load_map(cur_frm, 13)
-               }
+                }
             }
     });
-});
+}
 
 function load_map(cur_frm, zoom){
     data = get_args_details(cur_frm, zoom)
